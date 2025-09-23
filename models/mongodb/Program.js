7d569da4103607d973be4b8b2a0206559d65b7e3
Yemsrach <yemsrachtessema47@gmail.com>
2025-09-23 15:48:44 +0200

    change in program

--- models/mongodb/Program.js
+++ models/mongodb/Program.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 const programSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: String,
   startTime: Date,
   endTime: Date,
-  speakers: [String]
-},{
-    timestamps: true,
-    versionKey: false
-  });
+  description: String,
+  speakers: [{ fullName: String, credential: String }]
+}, {
+  timestamps: true,
+  versionKey: false
+});
 
 export default mongoose.model('Program', programSchema);
